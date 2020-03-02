d225875b809ba78e21894833d936e51caecadaba
Novik <novik65@gmail.com>
2020-03-02 18:37:56 +0300

    fix delivery mail

--- app/controllers/spree/api/shipments_controller_decorator.rb
+++ app/controllers/spree/api/shipments_controller_decorator.rb
@@ -2,16 +2,16 @@ Spree::Api::ShipmentsController.class_eval do
 
   def ship
     unless @shipment.shipped?
-      @shipment.ship!
       if @shipment.shipping_method.boxberry?
         tracking = ::Boxberry::API::create_delivery( @shipment )
         if tracking.present?
-          @shipment.update_columns(tracking: tracking)
+          @shipment.update(tracking: tracking)
           flash[:success] = Spree.t(:parsel_boxberry_create_success)
         else
           flash[:error] = Spree.t(:parsel_boxberry_create_fail, reason: ::Boxberry::API::last_error_message)
         end
       end
+      @shipment.ship!
     end
     respond_with(@shipment, default_template: :show)
   end
